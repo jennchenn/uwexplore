95fb5dab544d8d1e8c22d86a69216dba1daa924d
Yzabelle Go <ytgo@uwaterloo.ca>
2023-01-30 20:49:35 -0500

    basic page structure resolved yarn.lock conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,36 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Stack from "@mui/material/Stack";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Navbar></Navbar>
+      <Grid container>
+        <Grid xs={4} style={{ background: "purple", height: "95vh" }}>
+          Search
+        </Grid>
+        <Grid xs={8} style={{ background: "pink", height: "95vh" }}>
+          <Stack>
+            <div style={{ background: "blue" }}>Calendar</div>
+            <div
+              style={{
+                background: "green",
+                position: "fixed",
+                bottom: "0",
+                width: "100%",
+              }}
+            >
+              Ceab
+            </div>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
