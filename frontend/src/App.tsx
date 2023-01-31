c414bdff9cbea49fba0d461685b1d036fa77643a
Yzabelle Go <ytgo@uwaterloo.ca>
2023-01-30 21:03:41 -0500

    commented out unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./App.css";
+// import React from "react";
+// import "./App.css";
 
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
