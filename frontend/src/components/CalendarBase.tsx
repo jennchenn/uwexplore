f9bf221020d92f7f87250f132ec0debcb645da90
Yzabelle Go <52092038+yzzy2go@users.noreply.github.com>
2023-02-12 00:43:20 -0500

    updated with correct file

--- frontend/src/components/CalendarBase.tsx
+++ frontend/src/components/CalendarBase.tsx
@@ -1,84 +1,109 @@
-import "./App.css";
-import { useState } from "react";
+import { useMemo, useCallback, useState } from "react";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Unstable_Grid2";
-import Stack from "@mui/material/Stack";
-import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
-import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import moment from "moment";
+import "./CalendarBase.scss";
+import events from "../APIClients/events";
+import CalendarModal from "./CalendarModal";
 
-import Navbar from "./components/Navbar";
-import Search from "./components/Search";
-import Calendar from "./components/CalendarBase";
+const ReactBigCalendar = require("react-big-calendar");
+const { Calendar, momentLocalizer } = ReactBigCalendar;
 
-const sectionSizes = {
-  default: { search: 4, calendar: 8 },
-  allCal: { search: 0, calendar: 12 },
-};
+export default function CalendarBase() {
+  // localizer is required
+  const localizer = momentLocalizer(moment);
 
-function App() {
-  const [searchWidth, setSearchWidth] = useState(sectionSizes.default.search);
-  const [calendarWidth, setCalendarWidth] = useState(
-    sectionSizes.default.calendar,
+  // for popup modal when clicking a course on the calendar
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalTitle, setmodalTitle] = useState("placeholder title");
+  const [modalInfo, setmodalInfo] = useState("placeholder info");
+
+  const handleCourseSelectEvent = useCallback((event: HTMLTextAreaElement) => {
+    setmodalTitle(event.title);
+    setmodalInfo(event.value);
+    setModalOpen(true);
+  }, []);
+
+  const eventStyleGetter = (event: HTMLTextAreaElement) => {
+    let style = {
+      backgroundColor: courseColors[event.title],
+      border: "0px",
+    };
+    return {
+      style: style,
+    };
+  };
+
+  // colour options for courses on calendar
+  const availableColors = useMemo(() => {
+    const availableBackgroundColors = [
+      "#ff6961",
+      "#ffb480",
+      "#e5d54a",
+      "#42d6a4",
+      "#08cad1",
+      "#59adf6",
+      "#9d94ff",
+      "#c780e8",
+    ];
+    return availableBackgroundColors;
+  }, []);
+
+  const courseColors = useMemo(() => {
+    const results: any = {};
+    let colorMap = new Map();
+    let colorIndex = 0;
+    for (let event of events) {
+      let title = event.title;
+      if (!colorMap.has(title)) {
+        // Get a unique hex color for this title
+        colorMap.set(
+          title,
+          availableColors[colorIndex++ % availableColors.length],
+        );
+      }
+      results[event.title] = colorMap.get(title);
+    }
+    return results;
+  }, [availableColors]);
+
+  const { defaultDate, formats, views } = useMemo(
+    () => ({
+      // arbitrary starting date set to Jan, 1, 2023 - has an effect on dates used to set events
+      defaultDate: new Date(2023, 0, 1),
+      formats: {
+        dayFormat: (date: Date) => localizer.format(date, "ddd"),
+      },
+      views: ["work_week"],
+    }),
+    [localizer],
   );
-  const [sectionInView, setSectionInView] = useState("both");
-
-  const collapseSearch = () => {
-    setSearchWidth(sectionSizes.allCal.search);
-    setCalendarWidth(sectionSizes.allCal.calendar);
-    setSectionInView("calendar");
-  };
-
-  const expandSearch = () => {
-    setSearchWidth(sectionSizes.default.search);
-    setCalendarWidth(sectionSizes.default.calendar);
-    setSectionInView("both");
-  };
 
   return (
-    <Box>
-      <Navbar></Navbar>
-      <Grid container>
-        <Grid xs={searchWidth} className="search-base">
-          <Box
-            style={{
-              maxHeight: "92vh",
-              overflow: "auto",
-              position: "sticky",
-              top: "8vh",
-            }}
-          >
-            <KeyboardDoubleArrowLeftIcon
-              className="search-collapse-icon"
-              onClick={collapseSearch}
-            ></KeyboardDoubleArrowLeftIcon>
-            {/* todo: will this keep results? something better than empty tag? optimized? */}
-            {sectionInView === "calendar" ? <></> : <Search />}
-          </Box>
-        </Grid>
-        <Grid xs={calendarWidth} className="calendar-base">
-          <Box
-            style={{
-              maxHeight: "92vh",
-              overflow: "auto",
-              position: "sticky",
-              top: "8vh",
-            }}
-          >
-            <Stack direction="column">
-              <KeyboardDoubleArrowRightIcon
-                style={{
-                  display: sectionInView === "both" ? "none" : "inline-block",
-                }}
-                className="cal-collapse-icon"
-                onClick={expandSearch}
-              ></KeyboardDoubleArrowRightIcon>
-              <Calendar />
-            </Stack>
-          </Box>
-        </Grid>
-      </Grid>
+    <Box className="box-style">
+      <div className="calendar-height">
+        <Calendar
+          defaultDate={defaultDate}
+          defaultView={views}
+          events={events}
+          eventPropGetter={eventStyleGetter}
+          formats={formats}
+          localizer={localizer}
+          // shows time from 8AM to 10PM
+          min={new Date(0, 0, 0, 8, 0, 0)}
+          max={new Date(0, 0, 0, 22, 0, 0)}
+          onSelectEvent={handleCourseSelectEvent}
+          toolbar={false}
+          views={views}
+        ></Calendar>
+        <CalendarModal
+          modalTitle={modalTitle}
+          modalInfo={modalInfo}
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+          courseColors={courseColors}
+          availableBackgroundColors={availableColors}
+        ></CalendarModal>
+      </div>
     </Box>
   );
 }
-
-export default App;
