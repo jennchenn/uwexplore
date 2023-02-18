8aba17307e2d71f4c693b77d37a7374f133d6dc0
Bernie Cheng <46865131+BernadetteCheng@users.noreply.github.com>
2023-02-17 19:20:45 -0500

    tested scrape functions

--- backend/app/services/scrape.py
+++ backend/app/services/scrape.py
@@ -1,32 +1,76 @@
 from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
-import pandas as pd
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import re
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
-easyRating = ""  # Easy percentage rating
-usefulRating = ""  # Useful percentage rating
-likedRating = ""  # Liked percentage rating
-commentsCount = 0  # Number of comments
-comments = []  # List of class ratings
-ratingsCount = 0  # Number of ratings
-course = "cs115"
-driver.get("https://uwflow.com/course/" + course)
+def get_course_reviews(course):
+    url = "https://uwflow.com/course/" + course
 
-content = driver.page_source
-soup = BeautifulSoup(content)
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--incognito")
 
-for ratingWrapper in soup.findAll('div', attrs={'class': 'sc-qXhiz cejBIe'}):
-    easyRating = ratingWrapper.find('div', attrs={'class': 'sc-pcJja jjDvpo'}).text
-    usefulRating = ratingWrapper.find('div', attrs={'class': 'sc-pcJja jjDvpo'}).text
-    likedRating = ratingWrapper.find('div', attrs={'class': 'sc-psQdR iYtUny'}).text
-    commentsCount = int(ratingWrapper.find('a', attrs={'class': 'sc-qPwPv gjSZrg'}).text[0])
-    ratingsCount = int(ratingWrapper.find('div', attrs={'class': 'sc-qXRQq hXXFXN'}).text[0])
+    driver = webdriver.Chrome("chromedriver", options=options)
+    driver.get(url)
+    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".sc-qXRQq.hXXFXN")))
 
-if commentsCount > 0:
-    for commentCard in soup.findAll('div', attrs={'class': 'sc-pcZJD cnFfnA'}):
-        item = {}
-        item.comment = commentCard.find('div', attrs={'class': 'sc-pLwIe kqSAIH'}).text
-        item.program = commentCard.find('div', attrs={'class': 'sc-pTWqp kvWxGq'}).text[0]
-        item.prof = commentCard.find('a', attrs={'class': 'sc-qWQHW jhagES'}).text
-        comments.append(item)
+    content = driver.page_source
+    soup = BeautifulSoup(content, 'html.parser')
+
+    rating = soup.find_all('div', attrs={'class': 'sc-pcJja jjDvpo'})
+    easy_rating = rating[0].text
+    useful_rating = rating[1].text
+    liked_rating = soup.find('div', attrs={'class': 'sc-psQdR iYtUny'}).text
+    comments_count = int(re.findall(r'\d+', soup.find('a', attrs={'class': 'sc-qPwPv gjSZrg'}).text)[0])
+    ratings_count = int(re.findall(r'\d+', soup.find('div', attrs={'class': 'sc-qXRQq hXXFXN'}).text)[0])
+    comments = []
+
+    if comments_count > 0:
+        # get top comments on the page
+        for commentCard in soup.findAll('div', attrs={'class': 'sc-pcZJD cnFfnA'}):
+            item = {
+                'comment': commentCard.find('div', attrs={'class': 'sc-pLwIe kqSAIH'}).text if commentCard.find('div',
+                                                                                                                attrs={
+                                                                                                                    'class': 'sc-pLwIe kqSAIH'}) else "",
+                'author': commentCard.find('div', attrs={'class': 'sc-pTWqp kvWxGq'}).text if commentCard.find('div',
+                                                                                                               attrs={
+                                                                                                                   'class': 'sc-pTWqp kvWxGq'}) else ""}
+            comments.append(item)
+
+    driver.quit()
+    return easy_rating, useful_rating, liked_rating, comments_count, ratings_count, comments
+
+
+def get_course_term_offered(course):
+    url = "https://uwflow.com/course/" + course
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--incognito")
+
+    driver = webdriver.Chrome("chromedriver", options=options)
+    driver.get(url)
+    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".sc-pAyMl.dPzwlT")))
+
+    content = driver.page_source
+    soup = BeautifulSoup(content, 'html.parser')
+
+    summary = soup.find('div', attrs={'class': 'sc-pAyMl dPzwlT'}).text
+    terms = summary.split("Offered: ", 1)[1][:-1]
+
+    driver.quit()
+    return terms
