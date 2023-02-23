d4dfaa19631447822156da0425a7842dd2b4728c
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2023-02-23 13:12:01 -0500

    Support app login via middleware (#20)
    * Create login via email and password and token refresh routes
    
    * Add authentication middleware to example course route
    
    * Add reference to BP starter code
    
    * Remove extra print statement
    
    * Update User model and functions to match new fields

--- backend/app/routes/course_routes.py
+++ backend/app/routes/course_routes.py
@@ -1,7 +1,6 @@
-import json
-
 from flask import Blueprint, current_app, jsonify, request
 
+from ..middlewares.authentication import require_login
 from ..services.course_service import CourseService
 
 course_service = CourseService(current_app.logger)
@@ -13,9 +12,18 @@ blueprint = Blueprint("course", __name__, url_prefix="/courses")
 def get_courses():
     try:
         result = course_service.get_courses()
-        print(result)
         return jsonify(result), 200
 
+    except Exception as e:
+        error_message = getattr(e, "message", None)
+        return jsonify({"error": (error_message if error_message else str(e))}), 500
+
+
+@blueprint.route("/saved", methods=["GET"], strict_slashes=False)
+@require_login
+def get_saved_courses():
+    try:
+        return jsonify({"success": "ok"}), 200
     except Exception as e:
         error_message = getattr(e, "message", None)
         return jsonify({"error": (error_message if error_message else str(e))}), 500
