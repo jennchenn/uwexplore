a85e54f16b2c0af327e72daf148368b7a3a8cacb
Bernie Cheng <46865131+BernadetteCheng@users.noreply.github.com>
2023-02-26 21:03:51 -0500

    css constants and separate file

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -1,4 +1,5 @@
 import { Box, Modal, Typography } from "@mui/material";
+import "../styles/LoginModal.css";
 
 type LoginModalProps = {
   modalTitle: string;
@@ -10,36 +11,19 @@ type LoginModalProps = {
 };
 
 export default function LoginModal({ modalOpen = true, ...LoginModalProps }) {
-  const modalStyle = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    borderRadius: "12px",
-    boxShadow: 24,
-    p: 4,
-  };
-
   return (
     <Modal
       open={modalOpen}
       onClose={LoginModalProps.handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
+      aria-labelledby="login-modal-title"
+      aria-describedby="login-modal-description"
     >
-      <Box sx={modalStyle}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {LoginModalProps.modalTitle}
+      <Box className="login-modal">
+        <Typography id="login-modal-title" variant="h5">
+          {LoginModalProps.modalTitle} THIS
         </Typography>
-        <Typography
-          style={{ whiteSpace: "pre-line" }}
-          id="modal-modal-description"
-          sx={{ mt: 2 }}
-        >
-          {LoginModalProps.modalInfo}
+        <Typography id="login-modal-description">
+          {LoginModalProps.modalInfo} lorel
         </Typography>
       </Box>
     </Modal>
