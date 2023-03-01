00e5eabbf68704a0402f960bd6b4f0b8d3686cbd
Yzabelle Go <ytgo@uwaterloo.ca>
2023-02-28 19:27:22 -0500

    ceab base layout, progressbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
@@ -9,6 +9,7 @@ import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrow
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import Calendar from "./components/CalendarBase";
+import Ceab from "./components/CeabBase";
 
 const sectionSizes = {
   default: { search: 5, calendar: 7 },
@@ -73,6 +74,7 @@ function App() {
                 onClick={expandSearch}
               ></KeyboardDoubleArrowRightIcon>
               <Calendar />
+              <Ceab />
             </Stack>
           </Box>
         </Grid>
---
+++ frontend/src/components/ProgressBar.tsx
@@ -0,0 +1,28 @@
+import "../styles/ProgressBar.css";
+
+const ProgressBar = (props: any) => {
+  const { completed, label, total } = props;
+
+  return (
+    <div className="component-container">
+      <div className="label-styles">{label}</div>
+      <div className="container-styles">
+        <div
+          className="filler-styles"
+          style={{
+            width: `${completed > total ? 100 : (completed / total) * 100}%`,
+            backgroundColor: `${
+              completed > total ? "var(--ceab-completed)" : "var(--primary)"
+            }`,
+          }}
+        ></div>
+        <div
+          className="bar-label-styles"
+          style={{ color: `${completed > total / 2 ? "white" : "black"}` }}
+        >{`${completed}/${total}`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
