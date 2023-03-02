ff809ac4eb8c56bf54f8ce51c1987baac9836bfc
Bernie Cheng <46865131+BernadetteCheng@users.noreply.github.com>
2023-03-02 15:21:19 -0500

    clean

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -7,15 +7,13 @@ type TextInputProps = {
 
 export default function TextInput({ type = "text", ...TextInputProps }) {
   return (
-    <>
-      <input
-        type={type}
-        className="text-input heading-4"
-        placeholder={TextInputProps.placeholder}
-        style={{ ...TextInputProps.styles }}
-      >
-        {TextInputProps.value}
-      </input>
-    </>
+    <input
+      type={type}
+      className="text-input heading-4"
+      placeholder={TextInputProps.placeholder}
+      style={{ ...TextInputProps.styles }}
+    >
+      {TextInputProps.value}
+    </input>
   );
 }
