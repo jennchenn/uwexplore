7d0e630eef8c3765fd9fadd8a2bd44b2caf9d6a7
Bernie Cheng <46865131+BernadetteCheng@users.noreply.github.com>
2023-03-02 20:37:44 -0500

    revised textinput to use mui

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -1,19 +1,54 @@
-type TextInputProps = {
+import { styled, TextField } from "@mui/material";
+import { Props } from "../App";
+
+interface TextInputProps extends Props {
   placeholder?: string;
   value?: string;
-  styles?: React.CSSProperties;
-  type?: React.HTMLInputTypeAttribute;
-};
+  required?: boolean;
+  focused?: boolean;
+}
 
-export default function TextInput({ type = "text", ...TextInputProps }) {
+export default function TextInput({
+  required = false,
+  ...TextInputProps
+}: TextInputProps) {
+  const StyledTextInput = styled(TextField)({
+    width: "-webkit-fill-available",
+    margin: "8px 72px",
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        border: "none",
+        backgroundColor: "none",
+      },
+      "& input": {
+        padding: "19px 32px",
+        height: "19px",
+        color: "#000000",
+        borderRadius: "10px",
+        backgroundColor: "#F7F7F7",
+      },
+    },
+    "& .MuiFormLabel-root": {
+      fontSize: "16px",
+      lineHeight: "19px",
+      fontFamily: "'Inter'",
+      fontStyle: "normal",
+      letterSpacing: "-0.03em",
+      padding: "0",
+      left: "17px",
+      top: "2px",
+      color: "#000000",
+      opacity: "0.6",
+    },
+  });
+
   return (
-    <input
-      type={type}
-      className="text-input heading-4"
-      placeholder={TextInputProps.placeholder}
-      style={{ ...TextInputProps.styles }}
-    >
-      {TextInputProps.value}
-    </input>
+    <StyledTextInput
+      required={required}
+      type="outlined"
+      className="styled-text-input heading-4"
+      label={TextInputProps.placeholder}
+      defaultValue={TextInputProps.value}
+    />
   );
 }
