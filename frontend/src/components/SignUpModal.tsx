392cd4f0fcfbf483731be1a792c15e26b1b26cf0
Bernie Cheng <46865131+BernadetteCheng@users.noreply.github.com>
2023-03-06 15:14:43 -0500

    basic sign up modal and updated button styles

--- frontend/src/components/SignUpModal.tsx
+++ frontend/src/components/SignUpModal.tsx
@@ -1,28 +1,73 @@
 import { useState } from "react";
 import { Box, Modal } from "@mui/material";
 import { Props } from "../App";
-import "../styles/SignUpModal.css";
+import TextInput from "./TextInput";
+import "../styles/LoginSignUpModal.css";
+import CustomButton from "./CustomButton";
 
 interface SignUpModalProps extends Props {
-  modalTitle?: string;
-  modalInfo?: string;
-  open?: boolean;
-  buttonText?: string;
+  open: boolean;
+  setOpen: (open: boolean) => any;
+  linkOpen: () => void;
 }
 
 export default function SignUp({
-  open = true,
+  open = false,
   ...SignUpModalProps
 }: SignUpModalProps) {
-  const [modalOpen, setModalOpen] = useState(open);
-
   const handleClose = () => {
-    setModalOpen(false);
+    SignUpModalProps.setOpen(false);
   };
 
+  const [email] = useState("");
+  const [password] = useState("");
+  const [confirmPassword] = useState("");
+
+  const inputStyles = { width: "-webkit-fill-available", margin: "8px 72px" };
+
   return (
-    <Modal open={modalOpen} onClose={handleClose}>
-      <Box className="sign-up-modal"></Box>
+    <Modal
+      className={SignUpModalProps.className}
+      open={open}
+      onClose={handleClose}
+      style={SignUpModalProps.style}
+    >
+      <Box className="sign-up-modal">
+        <div className="sign-up-modal-title heading-1">CREATE AN ACCOUNT</div>
+        <div className="sign-up-modal-description heading-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          sollicitudin dapibus nisi, quis eleifend felis pharetra vel. Mauris ac
+          iaculis mauris.
+        </div>
+        <TextInput
+          id="sign-up-email"
+          placeholder="Email"
+          type="email"
+          value={email}
+          required
+        />
+        <TextInput
+          id="sign-up-password"
+          placeholder="Password"
+          type="password"
+          value={password}
+          required
+        />
+        <TextInput
+          id="sign-up-password-confirm"
+          placeholder="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          required
+        />
+        <CustomButton text="create account" type="CTA" style={inputStyles} />
+        <CustomButton
+          text="already have an account? login"
+          type="tertiary"
+          style={{ paddingBottom: "0", ...inputStyles }}
+          onClick={SignUpModalProps.linkOpen}
+        />
+      </Box>
     </Modal>
   );
 }
