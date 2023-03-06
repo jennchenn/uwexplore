f9c81ce9d7fff1c39550e43a2b00713ff227b9be
Yzabelle Go <52092038+yzzy2go@users.noreply.github.com>
2023-03-06 00:43:10 -0500

    Added filtering menu (#39)
    * added filtering menu
    
    * remove background colour for now
    
    * small changes

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -11,9 +11,17 @@ import Typography from "@mui/material/Typography";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import InfoIcon from "@mui/icons-material/Info";
 import SearchIcon from "@mui/icons-material/Search";
+
+import FilteringMenu from "./FilteringMenu";
 import SearchCards from "./SearchCards";
 
 export default function Search() {
+  const [showFilterMenu, setShowFilterMenu] = useState(false);
+
+  const handleShowFilterMenu = () => {
+    setShowFilterMenu(!showFilterMenu);
+  };
+
   return (
     <div>
       <Box sx={{ m: 2 }}>
@@ -34,10 +42,18 @@ export default function Search() {
                 ),
               }}
             />
-            <Button variant="text" size="small" startIcon={<FilterAltIcon />}>
+            <Button
+              variant="text"
+              size="small"
+              startIcon={<FilterAltIcon />}
+              onClick={handleShowFilterMenu}
+            >
               Filter
             </Button>
           </Stack>
+          {showFilterMenu && (
+            <FilteringMenu setShowFilterMenu={setShowFilterMenu} />
+          )}
           {/* todo: conditionally show welcome card */}
           <Card sx={{ minWidth: 100 }}>
             <CardContent>
