da2e89939483806b21bfb2b3fb43d53bbbbd80f6
Bernie Cheng <46865131+BernadetteCheng@users.noreply.github.com>
2023-03-06 14:05:00 -0500

    linting fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "../styles/Navbar.css";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
