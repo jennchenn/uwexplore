1460a69b5625ff0e3a6c3f3013d6644fc4fde237
Jennifer Chen <jenn.chenn93@gmail.com>
2023-03-06 22:47:57 -0500

    Format course API client code

--- frontend/src/APIClients/CourseClient.tsx
+++ frontend/src/APIClients/CourseClient.tsx
@@ -1,28 +1,30 @@
-import APIClient from "./APIClient"
+import APIClient from "./APIClient";
 
 export type CourseObject = {
-    id: string;
-    name: string;
-    antirequisites: string[];
-    ceab_eng_design: number;
-    ceab_eng_sci: number;
-    ceab_math: number;
-    ceab_sci: number;
-    code: string;
-    course_id: string;
-    cse_weight: number;
-    department: string;
-    description: string;
-    description_abbreviated: string;
-    prerequisites: string[];
-    sections: object[];
-    tags: string[];
-}
+  id: string;
+  antirequisites: string[];
+  ceab_eng_design: number;
+  ceab_eng_sci: number;
+  ceab_math: number;
+  ceab_sci: number;
+  code: string;
+  course_id: string;
+  cse_weight: number;
+  department: string;
+  description_abbreviated: string;
+  description: string;
+  name: string;
+  prerequisites: string[];
+  sections: object[];
+  tags: string[];
+};
 
-export const getCourses = async(queryParams: string | null): Promise<CourseObject[]> => {
-    // FIXME: this should be thrown into a try catch
-    const { data } = await APIClient.get(`/courses${queryParams}`)
-    return data;
-}
+export const getCourses = async (
+  queryParams: string | null,
+): Promise<CourseObject[]> => {
+  // FIXME: this should be thrown into a try catch
+  const { data } = await APIClient.get(`/courses${queryParams}`);
+  return data;
+};
 
 export default { getCourses };
