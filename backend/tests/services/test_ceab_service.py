8411cd68ba338975bc40836db49025cc3de6df7e
Jennifer Chen <jenn.chenn93@gmail.com>
2023-03-06 23:31:58 -0500

    Update course service test to remove deleted imports

--- backend/tests/services/test_ceab_service.py
+++ backend/tests/services/test_ceab_service.py
@@ -4,7 +4,7 @@ import pytest
 from bson.objectid import ObjectId
 from flask import current_app
 
-from app.models.course import ClassType, Course, CourseType, Section, Weekday
+from app.models.course import Course, Section
 from app.models.schedule import Schedule, ScheduleCourses
 from app.models.user import PastCourses, User
 from app.services.ceab_service import CeabService
@@ -22,76 +22,118 @@ def ceab_service():
 
 DEFAULT_COURSES = [
     Course(
-        _id=ObjectId(),
         name="Fundamental Engineering Math 1",
         department="SYDE",
         code="111",
         description="Functions: trigonometric, exponential, log, inverse functions. Differential calculus: limits, continuity, derivatives, differentials, applications. Sequences and series: convergence, power series, Taylor expansions. Simple numerical methods. [Offered: F]",
         ceab_math=42.0,
+        course_id="000002",
         sections=[
             Section(
-                day=[Weekday.M, Weekday.W],
+                day=["MONDAY", "WEDNESDAY"],
                 term_code="1229",
+                term_name="Spring 2022",
                 start_time=510,
                 end_time=590,
                 class_number=4877,
                 location="E5 6008",
-                type=ClassType.LEC,
+                type="LEC",
                 number="001",
                 start_date=datetime.datetime(2023, 9, 7),
                 end_date=datetime.datetime(2023, 12, 17),
                 enrolled_number=102,
                 capacity=102,
+                course_id=ObjectId(),
+            ),
+            Section(
+                day=["TUESDAY", "THURSDAY"],
+                term_code="1229",
+                term_name="Spring 2022",
+                start_time=510,
+                end_time=590,
+                class_number=4878,
+                location="E5 6008",
+                type="LEC",
+                number="002",
+                start_date=datetime.datetime(2023, 9, 7),
+                end_date=datetime.datetime(2023, 12, 17),
+                enrolled_number=102,
+                capacity=102,
+                course_id=ObjectId(),
+            ),
+            Section(
+                day=["FRIDAY"],
+                term_code="1229",
+                term_name="Spring 2022",
+                start_time=510,
+                end_time=560,
+                class_number=4879,
+                location="E5 6008",
+                type="TUT",
+                number="001",
+                start_date=datetime.datetime(2023, 9, 7),
+                end_date=datetime.datetime(2023, 12, 17),
+                enrolled_number=102,
+                capacity=102,
+                course_id=ObjectId(),
             ),
         ],
     ),
     Course(
-        _id=ObjectId(),
         name="Fundamental Engineering Math 2",
         department="SYDE",
         code="112",
         description="Integration: Indefinite and definite integral; techniques of integration; improper integrals, numerical methods, applications. Vector differential calculus: Partial, total, and directional derivative; Gradient divergence and curl; Jacobian. Applications. [Offered: W, S]",
         ceab_math=42.0,
+        course_id="000003",
         sections=[
             Section(
-                day=[Weekday.M, Weekday.W],
+                day=["MONDAY", "WEDNESDAY"],
                 term_code="1229",
+                term_name="Spring 2022",
                 start_time=600,
                 end_time=680,
                 class_number=4977,
                 location="E5 6008",
-                type=ClassType.LEC,
+                type="LEC",
                 number="001",
                 start_date=datetime.datetime(2023, 9, 7),
                 end_date=datetime.datetime(2023, 12, 17),
                 enrolled_number=102,
                 capacity=102,
+                course_id=ObjectId(),
             ),
-        ],
-    ),
-    Course(
-        _id=ObjectId(),
-        name="Introduction to Machine Learning",
-        department="MSCI",
-        code="446",
-        description="This course covers algorithmic and statistical foundations of data mining: extracting previously unknown and useful information from data. Topics include exploratory data analysis, data cleaning, data transformations, association rule mining, and both supervised and unsupervised learning. Methods typically covered include, but are not limited to: the Apriori algorithm, Bayesian inference, decision trees, linear and logistic regression, nearest-neighbor classification, and k-means clustering. [Offered: W]",
-        ceab_eng_sci=25.2,
-        ceab_eng_design=16.8,
-        course_type=CourseType.TE,
-        sections=[
             Section(
-                day=[Weekday.T, Weekday.F],
+                day=["TUESDAY", "TUESDAY"],
                 term_code="1229",
-                start_time=900,
-                end_time=980,
-                class_number=4900,
-                location="E7 4757",
-                type=ClassType.LEC,
+                term_name="Spring 2022",
+                start_time=600,
+                end_time=680,
+                class_number=4978,
+                location="E5 6008",
+                type="LEC",
+                number="002",
+                start_date=datetime.datetime(2023, 9, 7),
+                end_date=datetime.datetime(2023, 12, 17),
+                enrolled_number=102,
+                capacity=102,
+                course_id=ObjectId(),
+            ),
+            Section(
+                day=["FRIDAY"],
+                term_code="1229",
+                term_name="Spring 2022",
+                start_time=600,
+                end_time=650,
+                class_number=4979,
+                location="E5 6008",
+                type="TUT",
                 number="001",
                 start_date=datetime.datetime(2023, 9, 7),
                 end_date=datetime.datetime(2023, 12, 17),
-                enrolled_number=120,
-                capacity=120,
+                enrolled_number=102,
+                capacity=102,
+                course_id=ObjectId(),
             ),
         ],
     ),
@@ -104,7 +146,6 @@ def seed_database():
     Course.objects.delete()
     course_ids = []
     for course in DEFAULT_COURSES:
-        course.save()
         course_ids.append(str(course._id))
     schedule_courses = []
     for course_oid in course_ids:
