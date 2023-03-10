ccf4d5be7a2d78b9bbbd82b677afd793088fd45e
Jennifer Chen <jenn.chenn93@gmail.com>
2023-03-10 11:38:55 -0500

    Add clients for getting calendar courses

--- frontend/src/APIClients/CourseClient.tsx
+++ frontend/src/APIClients/CourseClient.tsx
@@ -19,6 +19,26 @@ export type CourseObject = {
   tags: string[];
 };
 
+export type CalendarCourseObject = {
+  id: string;
+  antirequisites: string[];
+  ceab_eng_design: number;
+  ceab_eng_sci: number;
+  ceab_math: number;
+  ceab_sci: number;
+  code: string;
+  course_id: string;
+  cse_weight: number;
+  department: string;
+  description_abbreviated: string;
+  description: string;
+  name: string;
+  prerequisites: string[];
+  sections: object[];
+  tags: string[];
+  color: string;
+};
+
 const getCourses = async (
   queryParams: string | null,
 ): Promise<CourseObject[]> => {
@@ -27,7 +47,27 @@ const getCourses = async (
   return data;
 };
 
+const getCoursesOnCalendar = async (): Promise<CalendarCourseObject[]> => {
+  // FIXME: this should be thrown into a try catch
+  // FIXME: add authorization header! "Authorization: Bearer <>"
+  const bearerToken = "";
+  const { data } = await APIClient.get("/courses/schedule", {
+    headers: { Authorization: bearerToken },
+  });
+  return data;
+};
+
+const getCoursesByCalendarId = async (
+  id: string,
+): Promise<CalendarCourseObject[]> => {
+  // FIXME: this should be thrown into a try catch
+  const { data } = await APIClient.get(`/courses/schedules/${id}`);
+  return data;
+};
+
 const clients = {
   getCourses,
+  getCoursesOnCalendar,
+  getCoursesByCalendarId,
 };
 export default clients;
